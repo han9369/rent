e73bfec217d0ceebe733b0918f931fe24f227ab0
Han <gaohan.henry@Gmail.com>
2016-04-06 01:56:50 -0700

    modify welcome page

--- js/index.js
+++ js/index.js
@@ -1,33 +1,138 @@
 $(document).ready(function() {
 
-    $('.bu1').click(function() {
-        $('.welcome-page')
-        .css({
-          'display': 'none',
-        });
-
-        $('.rent')
-        .css({
-          'display': 'block',
-        });
-    });
-
-    $('.bu2').click(function() {
-        $('.welcome-page')
-        .css({
-          'display': 'none',
-        });
-
-        $('.rent')
-        .css({
-          'display': 'none',
-        });
-
-        $('.for-rent')
-        .css({
-          'display': 'block',
-        });
-    });
-
-
 }); 
+
+(function(){
+
+  var app = angular.module('people',[]);
+
+  app.controller('PeopleController',function(){
+      this.person = pro;
+      this.button = buttons;
+
+  });
+
+  var buttons = [
+    {
+      num: 0,
+      age: 21,
+      gender: true, //true = female
+      rate: 30,
+      photo: 'g1.jpeg',
+      city: 'Vancouver'
+    },
+    {
+      num: 1,
+      age: 21,
+      gender: true, //true = female
+      rate: 30,
+      photo: 'g1.jpeg',
+      city: 'Vancouver'
+    },{
+      num: 2,
+      age: 21,
+      gender: true, //true = female
+      rate: 30,
+      photo: 'g1.jpeg',
+      city: 'Vancouver'
+    },
+    {
+      num: 3,
+      age: 21,
+      gender: true, //true = female
+      rate: 30,
+      photo: 'g1.jpeg',
+      city: 'Vancouver'
+    },
+    {
+      num: 4,
+      age: 21,
+      gender: true, //true = female
+      rate: 30,
+      photo: 'g1.jpeg',
+      city: 'Vancouver'
+    }
+
+
+
+  ];
+
+  var pro = [
+    {
+      num: 0,
+      age: 21,
+      gender: true, //true = female
+      rate: 30,
+      photo: 'g1.jpeg',
+      city: 'Vancouver'
+    },
+    {
+      num: 1,
+      age: 22,
+      gender: false, //true = female
+      rate: 25,
+      photo: 'b1.jpeg',
+      city: 'Vancouver'
+    },
+    {
+      num: 2,
+      age: 20,
+      gender: true, //true = female
+      rate: 40,
+      photo: 'g2.jpeg',
+      city: 'Vancouver'
+    },
+    {
+      num: 3,
+      age: 21,
+      gender: true, //true = female
+      rate: 30,
+      photo: 'img/photo/g2.jpeg',
+      city: 'Vancouver'
+    },
+    {
+      num: 4,
+      age: 21,
+      gender: true, //true = female
+      rate: 30,
+      photo: 'img/photo/g1.jpeg',
+      city: 'Vancouver'
+    },
+    {
+      num: 5,
+      age: 21,
+      gender: true, //true = female
+      rate: 30,
+      photo: 'img/photo/g1.jpeg',
+      city: 'Vancouver'
+    },
+    {
+      num: 6,
+      age: 21,
+      gender: true, //true = female
+      rate: 30,
+      photo: 'img/photo/g1.jpeg',
+      city: 'Vancouver'
+    },
+    {
+      num: 7,
+      age: 21,
+      gender: true, //true = female
+      rate: 30,
+      photo: 'img/photo/g1.jpeg',
+      city: 'Vancouver'
+    },
+    {
+      num: 8,
+      age: 21,
+      gender: true, //true = female
+      rate: 30,
+      photo: 'img/photo/g1.jpeg',
+      city: 'Vancouver'
+    }
+
+  ];
+
+
+
+})();
